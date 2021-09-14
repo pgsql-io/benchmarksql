ab8fb9305c0f25280680da1c647bd09d49cd143e
Peter Geoghegan <pg@bowt.ie>
2021-09-14 09:34:04 -0700

    Make index on orders table more useful.

--- src/main/resources/sql.common/indexCreates.sql
+++ src/main/resources/sql.common/indexCreates.sql
@@ -15,7 +15,7 @@ alter table bmsql_oorder add constraint bmsql_oorder_pkey
   primary key (o_w_id, o_d_id, o_id);
 
 create unique index bmsql_oorder_idx1
-  on  bmsql_oorder (o_w_id, o_d_id, o_carrier_id, o_id);
+  on bmsql_oorder (o_w_id, o_d_id, o_c_id, o_id) INCLUDE (o_entry_d, o_carrier_id);
 
 alter table bmsql_new_order add constraint bmsql_new_order_pkey
   primary key (no_w_id, no_d_id, no_o_id);
